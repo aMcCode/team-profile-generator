e2750525c643ac904ce8532906026c9f82233b0a
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-06-26 15:10:33 -0400

    updated employee obj to return static value for getRole() per assignment instructions, created manager, engineer and intern objs and associated failing tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,14 +1,13 @@
 const Employee = require("../lib/Employee")
 
 //const emp = new Employee();
-const emp = new Employee("Alicia", 0, 'g@g.com', 'myRole');
+const emp = new Employee("Alicia", 0, 'g@g.com');
 
 test('can create an employee obj', () => {
     expect(emp.name).toBeTruthy();
     expect(emp.name).toEqual(expect.any(String));
     expect(emp.id).toEqual(expect.any(Number));
     expect(emp.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-    expect(emp.role).toEqual(expect.any(String));
 });
 
 test("can get employee's name", () => {
@@ -25,6 +24,5 @@ test("can get employee's email", () => {
 });
 
 test("can get employee's role", () => {
-    expect(emp.getRole()).toBeTruthy();
-    expect(emp.getRole()).toEqual(expect.any(String));
+    expect(emp.getRole()).toBe('Employee');
 });
---
+++ _tests_/Manager.test.js
@@ -0,0 +1,33 @@
+const Manager = require("../lib/Manager")
+
+const manager = new Manager();
+//const manager = new Manager("Alicia", 0, 'g@g.com', '1432M');
+
+test('can create a manager obj', () => {
+    expect(manager.name).toBeTruthy();
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+});
+
+test("can get manager's name", () => {
+    expect(manager.getName()).toBeTruthy();
+    expect(manager.getName()).toEqual(expect.any(String));
+});
+
+test("can get manager's ID", () => {
+    expect(manager.getId()).toEqual(expect.any(Number));
+});
+
+test("can get manager's email", () => {    
+    expect(manager.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+});
+
+test("can get manager's office number", () => {
+    expect(manager.getOfficeNum()).toBeTruthy();
+    expect(manager.getOfficeNum()).toEqual(expect.any(String));
+});
+
+test("can get manager's role", () => {
+    expect(manager.getRole()).toBe('Manager');
+});
