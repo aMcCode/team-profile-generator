330d4c1de0f342931fa315531fdeb7d752400f83
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-06-26 14:18:43 -0400

    coded employee object and associated failing tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require("../lib/Employee")
+
+//const employee = new Employee("Alicia", 0, 'g@g.com', 'myRole');
+const emp = new Employee();
+
+test('can create an employee obj', () => {
+    expect(emp.getName().toBeTruthy());
+    expect(emp.name).toEqual(expect.any(String));
+    expect(emp.id).toEqual(expect.any(Number));
+    expect(emp.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    expect(emp.role).toEqual(expect.any(String));
+});
+
+test("can get employee's name", () => {
+    expect(emp.getName().toBeTruthy());
+    expect(emp.getName()).toEqual(expect.any(String));
+});
+
+test("can get employee's ID", () => {
+    expect(emp.getId()).toEqual(expect.any(Number));
+});
+
+test("can get employee's email", () => {    
+    expect(emp.email).toMatch(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+});
+
+test("can get employee's role", () => {
+    expect(emp.getRole().toBeTruthy());
+    expect(emp.getRole()).toEqual(expect.any(String));
+});
